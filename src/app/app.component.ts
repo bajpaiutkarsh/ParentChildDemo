736304c8f4898a59e6658e3fff0ba9e98c8b6825
Utkarsh Bajpai <bajpai.utkarsh.28@gmail.com>
2020-08-21 11:21:43 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ViewChild, AfterViewInit } from "@angular/core";
 import { FormControl, FormGroup } from "@angular/forms";
 import { ChildComponent } from "./ChildComponent/child.component";
 
@@ -7,17 +7,30 @@ import { ChildComponent } from "./ChildComponent/child.component";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   @ViewChild(ChildComponent)
   childObj: ChildComponent;
-  emp_add = new FormGroup({
-    name: new FormControl(""),
-    email: new FormControl(""),
-    contact: new FormControl(""),
-    add: this.childObj.home_add
-  });
+  emp_add: FormGroup;
+  finalValue: any;
+  errorMsg: string;
+  constructor() {
+    this.emp_add = new FormGroup({
+      name: new FormControl(""),
+      email: new FormControl(""),
+      contact: new FormControl("")
+    });
+  }
+  ngAfterViewInit(): void {
+    this.emp_add.addControl("homeadd", this.childObj.home_add);
+  }
 
   onSubmit(value) {
-    console.log(this.emp_add.value);
+    this.errorMsg = "";
+    if (this.emp_add.valid) {
+      console.log(this.emp_add.value);
+      this.finalValue = JSON.stringify(this.emp_add.value);
+    } else {
+      this.errorMsg = "Put all required Values";
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { ChildComponent } from "./ChildComponent/child.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChildComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
